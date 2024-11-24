3614c74e82e39be6b4a87316456114ad8c8abc6e
Joaquín Medero <mederojoaquinelias@gmail.com>
2024-11-23 21:03:02 -0300

    Delete aplicandoLoAprendido4 directory

--- aplicandoLoAprendido4/funcionesControladoras.ts
+++
@@ -1,6 +0,0 @@
-export function comprobarTitulo(titulo:string):boolean{
-    return (titulo==="Sin titulo" || titulo==="");
-}
-export function comprobarDificultad(dificultad:number){
-    return ((isNaN(dificultad)) || (dificultad!=1 && dificultad!=2 && dificultad!=3));
-}
--- aplicandoLoAprendido4/funcionesPantalla.ts
+++
@@ -1,6 +0,0 @@
-export function escribirEnPantalla (cadena:string){
-    return console.log(`${cadena}`);
-}
-export function ingresoInvalido ():void{
-    escribirEnPantalla("Opción ingresada no válida.");
-}
